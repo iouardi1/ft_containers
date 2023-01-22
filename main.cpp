b86a40bc8820f69b36eedcc850eb7ce08339ff31
iouardi1 <ihssaneouardi2@gmail.com>
2023-01-22 20:27:52 +0100

    done with constructors

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include "vector.hpp"
 int main()
 {
-    std::vector<int> vec;
-    ft::vector<int> vec1;
+    std::vector<int>::iterator itr;
+    // std::vector<int> vec(5, 10);
+    // std::vector<int> vec1(vec.begin(), vec.end());
+    // ft::vector<int> vec(5, 10);
+    ft::vector<int> vec1(5, 10);
+    // ft::vector<int> vec1(5, 7);
 
-    std::vector<int>::value_type a;
-
+    std::cout << vec1[4] << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: iouardi <iouardi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 13:05:01 by iouardi           #+#    #+#             */
-/*   Updated: 2023/01/22 15:33:42 by iouardi          ###   ########.fr       */
+/*   Updated: 2023/01/22 19:36:42 by iouardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,50 @@ namespace ft
 	{
 		public:
 			typedef	T											value_type;
-			typedef	allocator									allocator_type;
+			typedef	size_t										size_type;
+			typedef	std::allocator<T>							allocator_type;
 			typedef	typename allocator_type::reference			reference;
 			typedef	typename allocator_type::const_reference	const_reference;
-			typedef	typename allocator_type::pointor			pointor;
-			typedef	typename allocator_type::const_pointor		const_pointor;
+			typedef	typename allocator_type::pointer			pointer;
+			typedef	typename allocator_type::const_pointer		const_pointer;
+		public://for constructors
+			explicit vector (const allocator_type& alloc = allocator_type()): size(0), capacity(0), arr(NULL), alloc(alloc)
+			{}
+			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()): size(n), capacity(n), alloc(alloc)
+			{
+				arr = this->alloc.allocate(n);
+				for (size_type i = 0; i < n; i++)
+					arr[i] = val;
+			}
+			template <class InputIterator>
+			vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+			{
+				size_type j = 0;
+				for (InputIterator i = first; i != last; i++)
+					j++;
+				size = j;
+				capacity = j;
+				this->alloc = alloc;
+				arr = this->alloc.allocate(j);
+				j = 0;
+				for (InputIterator i = first; i != last; i++)
+					arr[j++] = *i;
+			}
+		public://overloaded operator
+			const_reference operator[] (size_type n) const
+			{
+				return (arr[n]);
+			}
+			reference operator[] (size_type n)
+			{
+				return (arr[n]);
+			}
+			
 		private:
 			size_t		size;
 			size_t		capacity;
 			value_type	*arr;
-			Alloc		alloc;
-		
+			Alloc		alloc; 
 		
 
 	};
